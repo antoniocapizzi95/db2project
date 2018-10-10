1a818a932f15e3fcba96c21710dcf8fa382786d8
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2018-10-10 21:48:29 +0200

    second commit

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import OracleClass
 import Neo4jClass
-#n = Neo4jClass.Neo4jClass("bolt://localhost:7687","neo4j","prova")
-#n.print_greeting("prova")
+n = Neo4jClass.Neo4jClass("bolt://localhost:7687","neo4j","prova")
+n.print_greeting("prova")
 o = OracleClass.OracleClass()
 o.connect()
 
