4bd06b97e3136ccaae6e9f71a214cda1b740edab
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2018-10-24 19:41:41 +0200

    Refactoring classi per le query

--- QueryNeo4j.py
+++ QueryNeo4j.py
@@ -1,8 +1,8 @@
 from time import time
 import csv
 class QueryNeo4j(object) :
-    def query1(self,n4j):
-        query = "MATCH (n:Company) RETURN n"
+    def query1(self,n4j,queryNum):
+        query = self.chooseQuery(queryNum)
         l = []
         for i in range(31):
             before = time()
@@ -11,10 +11,23 @@ class QueryNeo4j(object) :
             result = (after - before) * 1000
             print(result)
             l.append(str(result))
-        self.writeCsv("Neo4j","1","100",l)
+        self.writeCsv("Neo4j",str(queryNum),"100",l)
+
+
+    def chooseQuery(self,number):
+        switcher = {
+            1: "",
+            2: "",
+            3: "",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July"
+        }
+        return switcher.get(number)
 
     def writeCsv(self,dbname,querynum,dataset,list):
         with open(dbname+"Results\\"+dbname+'Query'+querynum+'_'+dataset+'.csv', 'w') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            for row in list:
-                csvwriter.writerow(row)
+            file_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            file_writer.writerow(list)
--- QueryOracle.py
+++ QueryOracle.py
@@ -1,8 +1,8 @@
 from time import time
 import csv
 class QueryOracle(object) :
-    def query1(self, orac):
-        query = "select * from company"
+    def query(self, orac, queryNum):
+        query = self.chooseQuery(queryNum)
         l = []
         for i in range(31):
             before = time()
@@ -11,10 +11,23 @@ class QueryOracle(object) :
             result = (after - before) * 1000
             print(result)
             l.append(str(result))
-        self.writeCsv("Oracle","1","100",l)
+        self.writeCsv("Oracle",str(queryNum),"100",l)
+
+
+    def chooseQuery(self,number):
+        switcher = {
+            1: "Select * From Company WHERE date_immatriculation > '2014-10-01' AND date_immatriculation < '2015-04-01'",
+            2: "select denomination, ville.name as ville from company, addresse,ville where company.id_addresse = addresse.id and addresse.id_ville = ville.id",
+            3: "Select denomination, forme_juridique.name as forme_juridique, code_ape.name as code_ape from company,forme_juridique,code_ape where company.id_formjur = forme_juridique.id AND company.id_codeape = code_ape.id",
+            4: "April",
+            5: "May",
+            6: "June",
+            7: "July"
+        }
+        return switcher.get(number)
 
     def writeCsv(self,dbname,querynum,dataset,list):
         with open(dbname+"Results\\"+dbname+'Query'+querynum+'_'+dataset+'.csv', 'w') as csvfile:
-            csvwriter = csv.writer(csvfile)
-            for row in list:
-                csvwriter.writerow(row)
+            file_writer = csv.writer(csvfile, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+
+            file_writer.writerow(list)
