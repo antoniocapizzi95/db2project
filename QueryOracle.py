eb1b9bf56436f565314dd17ec01e6f76debf4e2b
Antonio Capizzi <antoniocapizzi95@hotmail.it>
2018-10-30 18:41:08 +0100

    aggiunta quinta query in oracle

--- QueryOracle.py
+++ QueryOracle.py
@@ -20,7 +20,7 @@ class QueryOracle(object) :
             2: "Select count(denomination), code_ape.name from company, code_ape where code_ape.id = company.ID_CODEAPE group by code_ape.name",
             3: "Select denomination, forme_juridique.name as forme_juridique, code_ape.name as code_ape from company,forme_juridique,code_ape where company.id_formjur = forme_juridique.id AND company.id_codeape = code_ape.id",
             4: "Select denomination, addresse.name as adresse,code_postal,ville.name as ville from company, addresse, ville, region where company.id_addresse = addresse.id and addresse.id_ville = ville.id and ville.id_region = region.id",
-            5: "May",
+            5: "Select forme_juridique.name as Form_juridique, count(company.id) as n from company, addresse, ville, region,forme_juridique Where company.id_formjur = forme_juridique.id AND company.id_addresse = addresse.id AND addresse.id_ville = ville.id AND ville.id_region = region.id and region.id= '532' GROUP BY forme_juridique.name",
             6: "June",
             7: "July"
         }
